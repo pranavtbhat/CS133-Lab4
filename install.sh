18f44ce96341560cb4af4e1a0461e705e36a2e0a
Pranav Thulasiram Bhat <pranavtbhat@gmail.com>
2017-05-16 19:10:20 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+# Fetch CUDA
 wget -O cuda.deb https://developer.nvidia.com/compute/cuda/8.0/Prod2/local_installers/cuda-repo-ubuntu1604-8-0-local-ga2_8.0.61-1_amd64-deb
+
+# Install CUDA
 sudo dpkg -i cuda.deb
 sudo apt-get update
+sudo apt-get install cuda
+
+# Install OpenCL
+sudo apt install ocl-icd-libopencl1 opencl-headers clinfo
