56672b5f89008fc9e600a42bb740f4d1c838d5d6
Pranav Thulasiram Bhat <pranavtbhat@gmail.com>
2017-05-16 19:02:42 -0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+wget -O cuda.deb https://developer.nvidia.com/compute/cuda/8.0/Prod2/local_installers/cuda-repo-ubuntu1604-8-0-local-ga2_8.0.61-1_amd64-deb
+sudo dpkg -i cuda.deb
+sudo apt-get update
